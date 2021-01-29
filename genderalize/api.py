7f4cca8c3222bb94474af8dd80f57593f784b70a
Chris Wallace <chris@bitcollider.io>
2021-01-29 22:22:12 +0000

    Close over model in REST API so it is not reloaded on each request
    The previous process_text function was totally unnecessary. Now load model once on app start.

--- genderalize/api.py
+++ genderalize/api.py
@@ -32,17 +32,10 @@ class Gender(str, Enum):
 
 
 app = FastAPI()
-
-
-def process_text(text: str):
-    nlp = en_core_web_sm.load()
-
-    gender_component = GenderMatcher(nlp, gendered_dict)
-    nlp.add_pipe(gender_component)
-    nlp.add_pipe(gender_generalization_matcher)
-
-    doc = nlp(text)
-    return doc, nlp
+nlp = en_core_web_sm.load()
+gender_component = GenderMatcher(nlp, gendered_dict)
+nlp.add_pipe(gender_component)
+nlp.add_pipe(gender_generalization_matcher)
 
 
 @app.get("/status")
@@ -52,13 +45,13 @@ def status():
 
 @app.post("/biases", response_model=Biases)
 def biases(text: Text):
-    doc, _ = process_text(text.text)
+    doc = nlp(text.text)
     return {"generalization": doc._.is_gender_generalization}
 
 
 @app.post("/biases/generalizations/{gender}", response_model=Text)
 def generalization(text: Text, gender: Gender) -> Text:
-    doc, nlp = process_text(text.text)
+    doc = nlp(text.text)
     if doc._.is_gender_generalization:
         doc = replace_pronouns(nlp, doc, gender)
     return {"text": doc.text}
